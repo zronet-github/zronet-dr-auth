9eb0c560de16a4cc0d300200ff4d5a452a06d4e4
Dan ZroNet <dan@zronet.com>
2020-08-27 09:23:16 +0000

    Added encrypted file

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,62 +1,59 @@
-import { MongoMemoryServer} from 'mongodb-memory-server'
-import mongoose from 'mongoose'
-import { app } from '../app'
-import request from 'supertest'
-
-
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(): Promise<string[]>
-    }
-  }
+	namespace NodeJS {
+		interface Global {
+			signin(): Promise<string[]>;
+		}
+	}
 }
 
 let mongo: any;
 
-beforeAll(async () => { // run before testing: setup database connection
-  process.env.JWT_KEY = 'ntim8081583'
-   mongo = new MongoMemoryServer()
-  const mongoUri = await mongo.getUri()
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-})
+beforeAll(async () => {
+	// run before testing: setup database connection
+	process.env.JWT_KEY = 'key8686';
+	mongo = new MongoMemoryServer();
+	const mongoUri = await mongo.getUri();
+	await mongoose.connect(mongoUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+});
 
-beforeEach(async () => { // run before each test: delete all collections 
-  const collections = await mongoose.connection.db.collections()
+beforeEach(async () => {
+	// run before each test: delete all collections
+	const collections = await mongoose.connection.db.collections();
 
-  for (let collection of collections) {
-    await collection.deleteMany({})
-  }
+	for (let collection of collections) {
+		await collection.deleteMany({});
+	}
+});
 
-})
+afterAll(async () => {
+	//stop mongo and close connection after all tests
+	await mongo.stop();
+	await mongoose.connection.close();
+});
 
+global.signin = async () => {
+	// create global function instead of importing
 
-afterAll(async () => { //stop mongo and close connection after all tests
-  await mongo.stop()
-  await mongoose.connection.close()
-})
+	const email = 'test@test.com';
+	const password = 'password';
 
-global.signin = async () => { // create global function instead of importing
+	const response = await request(app)
+		.post('api/users/signup')
+		.send({
+			email,
+			password
+		})
+		.expect(201);
 
-  const email = 'test@test.com'
-  const password = 'password'
+	const cookie = response.get('Set-Cookie');
 
-  const response = await request(app) 
-  .post('api/users/signup')
-  .send({
-    email, password
-
-  })
-  .expect(201)
-
-
-  const cookie = response.get('Set-Cookie')
-
-  return cookie
-
-
-}
+	return cookie;
+};
